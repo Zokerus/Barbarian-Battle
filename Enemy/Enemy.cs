5acc7a62faea7446e04746ba92e3b1c0121bcc39
Zokerus <Zokerus@users.noreply.github.com>
2024-01-15 23:02:23 +0100

    add enemy hit animation and damage
    despawn enemy on death

--- Enemy/Enemy.cs
+++ Enemy/Enemy.cs
@@ -5,13 +5,34 @@ public partial class Enemy : PathFollow3D
 {
 	[Export]
 	public float speed = 5.0f;
+	[Export]
+	public int max_health = 50;
 
 	private Base m_base;
+	private int m_health;
+	private AnimationPlayer m_animationPlayer;
+
+	public int health
+	{
+		get { return m_health; }
+		set
+		{
+			if (value < m_health) 
+			{ m_animationPlayer.Play("TakeDamage"); }
+			m_health = value;
+			if (m_health < 1)
+			{
+				QueueFree();
+			}
+		}
+	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		m_base = (Base)GetTree().GetFirstNodeInGroup("base");
+		m_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		health = max_health;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
