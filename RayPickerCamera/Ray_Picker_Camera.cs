73f1ccd5f74ad7872fa5044a5396de0b95797a9e
Zokerus <Zokerus@users.noreply.github.com>
2024-01-17 11:26:31 +0100

    adjust mouse click handle in camera
    gui inputs will not be forwarded to gameplay anymore

--- RayPickerCamera/Ray_Picker_Camera.cs
+++ RayPickerCamera/Ray_Picker_Camera.cs
@@ -6,39 +6,68 @@ public partial class Ray_Picker_Camera : Camera3D
 {
 	[Export]
 	public TurretManager turretMananger;
+	[Export]
+	public int turretCost = 100;
 
 	private RayCast3D m_rayCast;
+	private Bank m_bank;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 		m_rayCast = GetNode<RayCast3D>("RayCast3D");
+		m_bank = (Bank)GetTree().GetFirstNodeInGroup("bank");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        base._UnhandledInput(@event);
+
+        if(m_rayCast.IsColliding() && m_bank.Gold >= turretCost)
+        {
+            GodotObject collider = m_rayCast.GetCollider();
+            if (collider is GridMap gridMap)
+            {
+                if (Input.IsActionPressed("click"))
+                {
+                    Vector3 collisionPoint = m_rayCast.GetCollisionPoint();
+                    Vector3I cell = gridMap.LocalToMap(collisionPoint);
+                    if (gridMap.GetCellItem(cell) == 0)
+                    {
+                        gridMap.SetCellItem(cell, 1);
+                        turretMananger.BuildTurret(gridMap.MapToLocal(cell));
+                        m_bank.Gold = m_bank.Gold - turretCost;
+                    }
+                }
+            }
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 		Vector2 mousePos = GetViewport().GetMousePosition();
 		m_rayCast.TargetPosition = this.ProjectLocalRayNormal(mousePos) * 100.0f; ;
 		m_rayCast.ForceRaycastUpdate();
         
-        if (m_rayCast.IsColliding()) 
+        if (m_rayCast.IsColliding() && m_bank.Gold >= turretCost) 
 		{
 			Input.SetDefaultCursorShape(Input.CursorShape.PointingHand);
-			GodotObject collider = m_rayCast.GetCollider();
-			if (collider is GridMap gridMap)
-			{
-				if(Input.IsActionPressed("click"))
-				{
-					Vector3 collisionPoint = m_rayCast.GetCollisionPoint();
-					Vector3I cell = gridMap.LocalToMap(collisionPoint);
-					if (gridMap.GetCellItem(cell) == 0)
-					{
-						gridMap.SetCellItem(cell, 1);
-						turretMananger.BuildTurret(gridMap.MapToLocal(cell));
-					}
-				}
-            }
+			//GodotObject collider = m_rayCast.GetCollider();
+			//if (collider is GridMap gridMap)
+			//{
+			//	if(Input.IsActionPressed("click"))
+			//	{
+			//		Vector3 collisionPoint = m_rayCast.GetCollisionPoint();
+			//		Vector3I cell = gridMap.LocalToMap(collisionPoint);
+			//		if (gridMap.GetCellItem(cell) == 0)
+			//		{
+			//			gridMap.SetCellItem(cell, 1);
+			//			turretMananger.BuildTurret(gridMap.MapToLocal(cell));
+			//			m_bank.Gold = m_bank.Gold - turretCost;
+			//		}
+			//	}
+   //         }
 		}
 		else
 		{
