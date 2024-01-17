840f6dbd4822599749f40ad3345c4bc8a79ea37d
Zokerus <Zokerus@users.noreply.github.com>
2024-01-17 11:25:20 +0100

    add difficulty adjustments to enemy spawn

---
+++ Level/EnemyPath.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class EnemyPath : Path3D
+{
+	[Export]
+	public PackedScene enemyScene {  get; set; }
+	[Export]
+	public DifficultyManager difficultyManager;
+	[Export]
+	public CanvasLayer victoryLayer;
+
+	private Timer m_spawnTimer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		m_spawnTimer = GetNode<Timer>("SpawnTimer");
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void SpawnEnemy()
+	{
+		Enemy enemy = enemyScene.Instantiate<Enemy>();
+		enemy.max_health = (int)difficultyManager.GetEnemyHealth();
+		this.AddChild(enemy);
+		m_spawnTimer.WaitTime = difficultyManager.GetSpawnTime();
+		enemy.TreeExited += EnemyDefeated; 
+	}
+
+	public void StopSpawnEnemies()
+	{
+		m_spawnTimer.Stop(); 
+	}
+
+	public void EnemyDefeated()
+	{
+		if (m_spawnTimer.IsStopped())
+		{
+			foreach (Node pathObject in this.GetChildren())
+			{
+				if (pathObject is Enemy enemy)
+				{
+					return;
+				}
+			}
+			victoryLayer.Show();
+		}
+	}
+}
